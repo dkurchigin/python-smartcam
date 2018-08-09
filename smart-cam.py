30e2ed43770276e3b7cf65ad3f86d11e2e2686e5
dkurchigin <kurchigin.dmitry@yandex.ru>
2018-08-09 21:35:44 +0300

    try mistery ttys0

---
+++ smart-cam.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import serial
+import serial.tools.list_ports
+import re
+
+string = "Smartcam ready!"
+smartcam_flag = False
+
+comlist = serial.tools.list_ports.comports()
+connected = []
+
+
+def connect_to_cam(port):
+    ser = serial.Serial(port, 9600, timeout=1)
+
+    while True :
+        line = ser.readline()
+        print (line)
+        dec_line = line.decode("ascii","ignore")
+        print (dec_line)
+        if dec_line.find(string):
+            print ('Smartcam ready to work!')
+            smartcam_flag = True
+        if smartcam_flag == True:
+            ser.write(input().encode("ascii","ignore"))
+
+
+for element in comlist:
+  connected.append(element.device)
+  print (element.device)
+  connect_to_cam('/dev/ttyS0')
